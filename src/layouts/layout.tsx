050d7813d172a8a0d21caa34077edf3c16986d06
surandra meena <205122045@nitt.edu>
2024-07-19 03:37:26 +0530

    first commit

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,20 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
+type Props ={
+    children: React.ReactNode;
+    showHero?: boolean;
+}
+
+const Layout = ({ children, showHero = false }: Props) =>{
+    return(
+        <div className="felx flex-col min-h-screen">
+            <Header />
+            {showHero && <Hero/>}
+            <div className="container mx-auto flex-1 py-10">{children}</div>
+            <Footer/>
+        </div>
+    )
+};
+export default Layout
